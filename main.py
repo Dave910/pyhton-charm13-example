02f9d4ab2d12ef9df6c02b60f442e19a3ec1773c
David <Dave91088@gmail.com>
2018-10-01 22:11:15 -0400

    New Branch

--- main.py
+++ main.py
@@ -2,14 +2,10 @@
 
 
 def main(msg):
-	//another another comment
-	//Adding another comment to this file
-	//What is up people
-	//Adding the second to this file
-	//Print a message
-	//Another example
-   	 //Another test
+	//no longer need comments here
     print(msg)
 
+def secondary():
+    print("new function")
 main("Hello people")
 main ("Nothing much and you")
