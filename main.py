ca6370501a7a939964c037e53f2fda1ed7072cf9
David <Dave91088@gmail.com>
2018-10-01 22:29:18 -0400

    new branch

--- main.py
+++ main.py
@@ -2,10 +2,11 @@
 
 
 def main(msg):
-    //no longer need this branch 
+    //no longer need this branch
     print(msg)
 
 def secondary():
     print("new function")
+
 main("Hello people")
 main ("Nothing much and you")
